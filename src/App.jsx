378992862f277406e35845084e1795481a74a4fb
theosanderson <theo@theo.io>
2021-04-22 00:15:43 +0100

    use https

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ function App () {
           <NavBar />
           <AppContainer>
             <DataProvider
-              default_data_url="http://covince.vercel.app/data"
-              default_tiles_url='http://covince.vercel.app/tiles/Local_Authority_Districts__December_2019__Boundaries_UK_BUC.json'
+              default_data_url="https://covince.vercel.app/data"
+              default_tiles_url='https://covince.vercel.app/tiles/Local_Authority_Districts__December_2019__Boundaries_UK_BUC.json'
             >
               <Covid19 />
             </DataProvider>
