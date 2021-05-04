1a62c2e4e4d3b2dba4be869ad598dd4b3c64dd72
theosanderson <theo@theo.io>
2021-05-04 20:05:16 +0100

    GISAID footer

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,11 @@ function App () {
           <NavBar />
           <AppContainer>
             <Covince default_data_url="/data" default_tiles_url="/map.json" />
+
           </AppContainer>
+          <div className="text-center p-2">
+          GISAID data provided on this website are subject to <a className="underline" href="https://www.gisaid.org/registration/terms-of-use/">GISAID Terms and Conditions</a>.
+          </div>
         </Suspense>
       </QueryClientProvider>
     </>
