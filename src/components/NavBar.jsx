b76fb39d8171fcf7d403ae42fde5cba4ed2065d0
theosanderson <theo@theo.io>
2021-05-05 14:11:12 +0100

    updates

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -38,7 +38,9 @@ const NavBar = () => {
   <header className={classNames('transition-all duration-500 ease-in-out bg-gradient-to-r from-blue-900 to-blue-900', height)}>
    {ackOpen && <div className="fixed mx-5 bg-white z-50 p-8 mt-5 border-gray-700 rounded-md shadow-md">
     <div className="font-bold cursor-pointer text-right" onClick={x => setAckOpen(!ackOpen)}><AiFillCloseCircle className="inline-block" size="1.5em"/> Close</div>
-      <h2 className="font-bold">GISAID</h2>
+    <h2 className="font-bold pb-1">CovGlobe</h2>
+    <p className="pb-5">Find the source code and more information on contributors <a href="https://github.com/theosanderson/covglobe" className="underline">on GitHub</a>.</p>
+      <h2 className="font-bold pb-1">GISAID</h2>
     We would like to thank the GISAID Initiative<sup>1</sup> and are grateful to all of the data contributors, i.e. the Authors, the Originating laboratories responsible for obtaining the specimens, and the Submitting laboratories for generating the genetic sequence and metadata and sharing via the GISAID Initiative, on which this research is based. <br /><br />
           1. Elbe, S., and Buckland-Merrett, G. (2017) Data, disease and diplomacy: GISAID’s innovative contribution to global health. Global Challenges, 1:33-46. DOI: <a className="underline" href="https://doi.org/10.1002/gch2.1018">10.1002/gch2.1018</a> PMCID: <a className="underline" href="https://pubmed.ncbi.nlm.nih.gov/31565258/">31565258</a>
     </div>}
