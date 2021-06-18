e8357ea4f62824315952edce4381911729c2eec5
Theo Sanderson <theo@sndrsn.co.uk>
2021-06-18 14:17:55 +0100

    Cease vendoring covince

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import React, { Suspense } from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import React, { Suspense } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-import NavBar from './components/NavBar'
-import Spinner from './components/Spinner'
-import AppContainer from './components/AppContainer'
-import Covince from './Covince'
+import NavBar from "./components/NavBar";
+import Spinner from "./components/Spinner";
+import AppContainer from "./components/AppContainer";
+import Covince from "covince/src/Covince.jsx";
 
-const twentyFourHoursInMs = 1000 * 60 * 60 * 24
+const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
       refetchOnmount: false,
       refetchOnReconnect: false,
-      staleTime: twentyFourHoursInMs * 100
-    }
-  }
-})
+      staleTime: twentyFourHoursInMs * 100,
+    },
+  },
+});
 
 const Loading = () => (
-  <div className='fixed inset-0 grid place-content-center'>
-    <Spinner className='w-6 h-6 text-gray-500' />
+  <div className="fixed inset-0 grid place-content-center">
+    <Spinner className="w-6 h-6 text-gray-500" />
   </div>
-)
+);
 
-function App () {
+function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
@@ -32,16 +32,21 @@ function App () {
           <NavBar />
           <AppContainer>
             <Covince default_data_url="/data" default_tiles_url="/map.json" />
-
           </AppContainer>
           <div className="text-center p-2 mx-5 text-sm ">
-          GISAID data provided on this website are subject to <a className="underline" href="https://www.gisaid.org/registration/terms-of-use/">GISAID's Terms and Conditions</a>.
-
+            GISAID data provided on this website are subject to{" "}
+            <a
+              className="underline"
+              href="https://www.gisaid.org/registration/terms-of-use/"
+            >
+              GISAID's Terms and Conditions
+            </a>
+            .
           </div>
         </Suspense>
       </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
